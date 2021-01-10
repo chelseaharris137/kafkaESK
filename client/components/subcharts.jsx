6452941b6350580d08f8b311ed4dd986e9bd9597
chelseaharris137 <72273359+chelseaharris137@users.noreply.github.com>
2021-01-10 10:56:14 -0500

    reconfigured subchart component to maintain consistency

--- client/components/subcharts.jsx
+++ client/components/subcharts.jsx
@@ -1,51 +1,49 @@
 import React, { Component } from "react";
-import DoughnutChart from './DoughnutChart.jsx';
-import BarChart from './BarChart.jsx';
-import PolarChart from './PolarChart.jsx';
-import '../styles/subcharts.scss';
+import DoughnutChart from "./DoughnutChart.jsx";
+import BarChart from "./BarChart.jsx";
+import PolarChart from "./PolarChart.jsx";
+import "../styles/subcharts.scss";
 
-const SubCharts = (props) => {
-  return (
-    <div id="subcharts">
-      <div id="chart1">
-        <DoughnutChart />
-      </div>
-      <div>&nbsp;</div>
-      <div id="chart2">
-        <BarChart />
-      </div>
-      <div>&nbsp;</div>
-      <div id="chart3">
-        <PolarChart />
-      </div>
-    </div>
-  );
-};
-
-
-
-// class SubCharts extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//       return (
+// const SubCharts = (props) => {
+//   return (
 //     <div id="subcharts">
-//     <div id="chart1">
-//       <DoughnutChart />
+//       <div id="chart1">
+//         <DoughnutChart />
+//       </div>
+//       <div>&nbsp;</div>
+//       <div id="chart2">
+//         <BarChart />
+//       </div>
+//       <div>&nbsp;</div>
+//       <div id="chart3">
+//         <PolarChart />
+//       </div>
 //     </div>
-//     <div>&nbsp;</div>
-//     <div id="chart2">
-//       <BarChart />
-//     </div>
-//     <div>&nbsp;</div>
-//     <div id="chart3">
-//       <RadarChart />
-//     </div>
-//   </div>
-// );
-//   }
-// }
+//   );
+// };
+
+class SubCharts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="subcharts">
+        <div id="chart1">
+          <DoughnutChart />
+        </div>
+        <div>&nbsp;</div>
+        <div id="chart2">
+          <BarChart />
+        </div>
+        <div>&nbsp;</div>
+        <div id="chart3">
+          <PolarChart />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SubCharts;
